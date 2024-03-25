1db0312f2e5e3d95e523288937d4cac47767d298
JuaArr <julopezar@unal.edu.co>
2024-03-25 17:25:58 -0500

    .

--- src/P1.cpp
+++ src/P1.cpp
@@ -3,6 +3,6 @@
 int main(void){
 	int n_molecules, lattice_size, n_iterations, seed;
 	std::string ifile_name = "input/input.txt";
-	read_params(ifile_name, &nmolecules, &latticesize, &niterations, &seed);
+	read_params(ifile_name, &n_molecules, &lattice_size, &n_iterations, &seed);
 	return 0;
 }
