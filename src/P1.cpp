46a4c40b1c09e3f90cf13f6b1dfb1eb02e294038
JuaArr <julopezar@unal.edu.co>
2024-03-24 20:57:26 -0500

    24-03-2024

--- src/P1.cpp
+++ src/P1.cpp
@@ -1,10 +1,8 @@
 #include "read_params.hpp"
 
 int main(void){
-	int nmolecules, latticesize, niterations, seed;
+	int n_molecules, lattice_size, n_iterations, seed;
 	std::string ifile_name = "input/input.txt";
 	read_params(ifile_name, &nmolecules, &latticesize, &niterations, &seed);
-	std::cout << nmolecules << " " << latticesize << " " << niterations \
-			  << " " << seed << "\n";
 	return 0;
 }
